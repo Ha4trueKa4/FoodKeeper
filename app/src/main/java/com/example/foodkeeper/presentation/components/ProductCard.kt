cf4f91f73715e31d90e73a09630c40f4d3f08c13
Dean <kelemandean76@gamil.com>
2026-03-14 21:52:35 +0300

    Начало работы над приложением. Создание пакетов для clean arch. Создание карточки продукта.

--- app/src/main/java/com/example/foodkeeper/presentation/components/ProductCard.kt
+++ app/src/main/java/com/example/foodkeeper/presentation/components/ProductCard.kt
@@ -1,14 +1,11 @@
 package com.example.foodkeeper.presentation.components
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,16 +16,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.foodkeeper.presentation.model.Product
 
 
 @Composable
-fun ProductCard(modifier: Modifier = Modifier, product : Product) {
+fun ProductCard(product : Product) {
     Box(
         modifier = Modifier
             .padding(vertical = 10.dp, horizontal = 40.dp)
