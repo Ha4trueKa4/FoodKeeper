63b691f3c4f946d3ef8b2e1a24a1075f50d648e6
Dean <kelemandean76@gamil.com>
2026-03-16 21:50:06 +0300

    Начало работы с навигацией

---
+++ app/src/main/java/com/example/foodkeeper/presentation/navigation/Routes.kt
@@ -0,0 +1,12 @@
+package com.example.foodkeeper.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Routes {
+    @Serializable
+    data object Login : Routes()
+    @Serializable
+    data object Register : Routes()
+    @Serializable
+    data object Main : Routes()
+}
---
+++ app/src/main/java/com/example/foodkeeper/presentation/screens/RegisterScreen.kt
@@ -0,0 +1,12 @@
+package com.example.foodkeeper.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RegisterScreen(
+    modifier: Modifier = Modifier,
+    onNavigateTo : (String) -> Unit
+) {
+
+}
